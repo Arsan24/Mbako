fe408f161ea02b359b7118a0703aceb9167984d7
MSNatama <m.shafanatama@gmail.com>
2023-06-09 15:04:45 +0800

    Change service key

--- app/auth/dbfirestore.py
+++ app/auth/dbfirestore.py
@@ -2,6 +2,6 @@ import firebase_admin
 from firebase_admin import credentials, firestore
 
 # initialize Firebase Admin SDK
-cred = credentials.Certificate("auth/serviceAccountKey.json")
+cred = credentials.Certificate("auth/c23-ps170-key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
