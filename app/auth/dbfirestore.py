e62f8df466bd45b413d31592a780e4d0eb03d3fc
MSNatama <m.shafanatama@gmail.com>
2023-06-10 12:15:37 +0800

    Securing Service account key

--- app/auth/dbfirestore.py
+++ app/auth/dbfirestore.py
@@ -1,7 +1,16 @@
 import firebase_admin
 from firebase_admin import credentials, firestore
+import os
+import base64
+import json
+
+# Retrieve the base64-encoded service account key from the environment variable
+encoded_key = os.environ.get('SERVICE_ACCOUNT_KEY')
+
+# Decode the base64-encoded string and load it as JSON
+key_data = base64.b64decode(encoded_key)
+cred = credentials.Certificate(json.loads(key_data))
 
 # initialize Firebase Admin SDK
-cred = credentials.Certificate("auth/c23-ps170-key.json")
 firebase_admin.initialize_app(cred)
 db = firestore.client()
