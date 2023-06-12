76bb3efd87c4803f43d8cbe8618d8e066b2342cf
MSNatama <m.shafanatama@gmail.com>
2023-06-12 18:45:07 +0800

    Adding quantity to items attributes

--- app/routes/items.py
+++ app/routes/items.py
@@ -24,10 +24,11 @@ def get_items():
 async def create_item(
     image: str = Form(), 
     pname: str = Form(), 
-    price: int = Form()
+    price: int = Form(),
+    quantity: int = Form()
 ):
     
-    item = Item(image=image, pname=pname, price=price)
+    item = Item(image=image, pname=pname, price=price, quantity=quantity)
     item_data = item.dict()
     collection_ref = db.collection('items')
     doc_ref = collection_ref.document()
@@ -57,10 +58,11 @@ async def update_item(
     item_id: str, 
     image: str, 
     pname: str, 
-    price: int 
+    price: int,
+    quantity: int
 ):
  
-    item = Item(image=image, pname=pname, price=price)
+    item = Item(image=image, pname=pname, price=price, quantity=quantity)
     updated_item = item.dict()   
     item_ref = db.collection('items').document(item_id)
     item_ref.update(updated_item)
@@ -81,3 +83,32 @@ def delete_item(item_id: str):
         "error": False,
         "message": "Barang berhasil dihapus!"
     }
+
+@router.post("/api/items/{item_id}/buy")
+async def buy_item(item_id: str, quantity: int):
+    # Retrieve the item from the database
+    item_ref = db.collection('items').document(item_id)
+    item = item_ref.get()
+
+    if item.exists:
+        # Get the current quantity of the item
+        current_quantity = item.get('quantity', 0)
+
+        if current_quantity >= quantity:
+            # Calculate the new quantity after selling
+            new_quantity = current_quantity - quantity
+
+            # Update the quantity in the database
+            item_ref.update({'quantity': new_quantity})
+
+            return {
+                "error": False,
+                "message": f"Item berhasil dibeli, sisa stok: {new_quantity}"
+            }
+        else:
+            return {
+                "error": True,
+                "message": "Stok telah habis"
+            }
+    else:
+        raise HTTPException(status_code=404, detail="Barang tidak ditemukan")
